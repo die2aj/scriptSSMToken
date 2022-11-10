5bae09326c9710b2e219045d634ba4d3a1a5b050
Diego Araque <diegoaraquej@gmail.com>
2022-11-10 07:28:54 -0500

    add new changes

--- script.py
+++ script.py
@@ -1,15 +1,23 @@
 import boto3
 import os
+import requests
+import json
 
 os.environ['AWS_DEFAULT_REGION'] = 'us-west-2'
 os.environ['AWS_PROFILE'] = "default"
 
 ssm_client = boto3.client("ssm")
 
+response = requests.get("https://api.themoviedb.org/3/authentication/token/new?api_key=725374be1608b260795fe92a785532b0")
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
 new_string_parameter = ssm_client.put_parameter(
     Name='apiToken',
     Description='this my token',
-    Value='8a1f9d6cc8720d795d34e22f6c49761ff2fca62c',
+    Value=response.json().get("request_token"),
     Type='SecureString',
     Overwrite=True,
     Tier='Standard')
